829d0b8622131682e1af17d39e9c30b0cbe6bf17
João Victor Fonseca Assis <joaovictorfosecaassis@gmail.com>
2024-10-10 10:45:44 -0300

    :zap: sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,21 +1,30 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
+import { useMemo } from "react";
 
 const Sidebar = () => {
   const pathname = usePathname();
 
   return (
-    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col  justify-between  bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((item) => {
-          const isActive =
-            pathname === item.route || pathname.startsWith(`${item.route}/`);
+          // Verifica se pathname é válido e se está presente
+
+          const isActive = useMemo(
+            () =>
+              pathname &&
+              (pathname === item.route ||
+                pathname.startsWith(`${item.route}/`)),
+            [pathname, item.route]
+          );
 
           return (
             <Link
