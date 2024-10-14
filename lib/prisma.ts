10d0d185e225e8a0b3f5a62c845f0ea6ab8734e7
João Victor Fonseca Assis <joaovictorfosecaassis@gmail.com>
2024-10-14 11:35:01 -0300

    :card_file_box: Neon

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "@neondatabase/serverless";
+import dotenv from "dotenv";
+import ws from "ws";
 
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
 const prisma = new PrismaClient();
 
 export default prisma;
