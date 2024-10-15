cce26410be0ecf61c38f142eba91dd5afaa598e7
João Victor Fonseca Assis <joaovictorfosecaassis@gmail.com>
2024-10-15 13:57:14 -0300

    Neon

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,35 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prisma";
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
     const data = req.body;
-
-    // Verifique se os campos necessários estão presentes
-    if (
-      !data.city ||
-      !data.pib ||
-      !data.uf ||
-      !data.mayor ||
-      !data.population
-    ) {
-      return res
-        .status(400)
-        .json({ error: "Todos os campos são obrigatórios." });
-    }
-
-    // Normalize o nome da cidade
     data.city = data.city.trim().toLowerCase();
-
     try {
       const city = await prisma.city.create({ data });
       res.status(201).json(city);
     } catch (error) {
-      console.error("Erro ao salvar a cidade:", error);
-      res.status(500).json({ error: "Erro ao salvar a cidade." });
+      res.status(500).json({ error: "Erro ao salvar a cidade" });
     }
   } else {
     res.setHeader("Allow", ["POST"]);
