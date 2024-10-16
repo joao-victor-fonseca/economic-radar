32c3dfcc8536065c48d5eca82cc1770777ba32ed
João Victor Fonseca Assis <joaovictorfosecaassis@gmail.com>
2024-10-16 10:20:49 -0300

    :wrench: Prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 import { Pool } from "@neondatabase/serverless";
 import dotenv from "dotenv";
-import ws from "ws";
 
 dotenv.config();
 
-const connectionString = process.env.DATABASE_URL;
+// Verifica se o DATABASE_URL_UNPOOLED foi definido. Caso contrário, usa DATABASE_URL.
+const connectionString =
+  process.env.DATABASE_URL_UNPOOLED || process.env.DATABASE_URL;
+
+// Configura o pool de conexões para o Neon (se necessário, dependendo do ambiente).
 const pool = new Pool({ connectionString });
-const prisma = new PrismaClient();
+
+// Inicializa o Prisma Client com a URL correta
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: connectionString,
+    },
+  },
+});
 
 export default prisma;
